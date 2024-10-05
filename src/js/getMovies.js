ca802c1b2b2357305668bcbd55d1ac4dbf533fcb
irashovdra <109846925+irashovdra@users.noreply.github.com>
2024-10-05 11:55:52 -0500

    .

--- src/js/getMovies.js
+++ src/js/getMovies.js
@@ -1,10 +1,5 @@
 function getMovies() {
-  fetch("http://localhost:3000/movies", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
+  fetch("http://localhost:3000/movies")
     .then((response) => response.json())
     .then((data) => {
       displayMovies(data.movies);
@@ -14,20 +9,6 @@ function getMovies() {
     });
 }
 
-// function displayMovies(movies) {
-//   const movieList = document.querySelector(".movies-list");
-//   movieList.innerHTML = "";
-
-//   movies.forEach((movie) => {
-//     const movieItem = document.createElement("li");
-//     movieItem.classList.add("movie-item");
-//     movieItem.innerHTML = `
-// <h2>${movie.title} (${movie.year})</h2>
-// <img src="${movie.photo}" alt="${movie.title}" width="200">
-//         `;
-//     movieList.appendChild(movieItem);
-//   });
-// }
 const createMovieCard = (movies) => {
   const movieCard = movies
     .map((movie) => {
